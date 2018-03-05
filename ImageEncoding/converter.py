5ad9833a0003f627af94107ea78762ca0b7fc222
polsala <polsalalidon@gmail.com>
2018-03-05 15:55:55 +0100

    Lib t.

--- ImageEncoding/converter.py
+++ ImageEncoding/converter.py
@@ -2,12 +2,20 @@ from base64 import b64encode, b64decode
 from simplecrypt import encrypt, decrypt
 
 
+<<<<<<< Updated upstream
 class Converter():
     def __init__(self, password):
         self.password = password
 
     def encrypt(self, plain_text):
         return encrypt(self.password, b64encode(plain_text[::-1]))
+=======
+class Converter:
+
+    @staticmethod
+    def encrypt(plain_text, password):
+        return encrypt(password, b64encode(plain_text[::-1]))
+>>>>>>> Stashed changes
 
     @staticmethod
     def decrypt(cyfred_text, password):
